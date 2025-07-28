37fc2c151800fbb36b58bae531ae0a01c34bf3c3
Petr Baklanov <baklanovp@gmail.com>
2025-07-28 17:54:48 +0300

    -ADD: public interface

--- src/ryabenky.f90
+++ src/ryabenky.f90
@@ -1,10 +1,13 @@
 module ryabmod
     use rspline3d, only: spline3d_type, p_dim_3d
+    use rspline4d, only: spline4d_type, p_dim_4d
 
     implicit none
     private
 
     public :: spline3d_type, p_dim_3d
+    public :: spline4d_type, p_dim_4d
+
 
     character(len=*), parameter  :: mdl_name = 'ryabmod'
 
